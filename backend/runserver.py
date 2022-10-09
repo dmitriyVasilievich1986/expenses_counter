67acdb16c44ff9405b74bafd6298560edf4cd90d
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-10-09 08:44:25 +0200

    update runserver file

--- backend/runserver.py
+++ backend/runserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from django.core.management import execute_from_command_line
+from argparse import ArgumentParser, Namespace
 from django.db.utils import OperationalError
-from argparse import ArgumentParser
 from django.db import connections
 from dotenv import load_dotenv
 from os import environ
@@ -18,7 +18,7 @@ class Modes(Enum):
     runserver = "runserver"
 
 
-def parse_args():
+def parse_args(*args, **kwargs) -> Namespace:
     parser = ArgumentParser(description="simple script to start django project")
     parser.add_argument(
         "--mode",
@@ -30,31 +30,29 @@ def parse_args():
     return parser.parse_args()
 
 
-def load_enviroments():
+def load_enviroments(*args, **kwargs) -> None:
     load_dotenv()
     load_dotenv("config.env")
 
 
-def execute_migrations(is_need_exit: bool):
+def execute_migrations(*args, **kwargs) -> None:
     execute_from_command_line([__name__, "makemigrations"])
     execute_from_command_line([__name__, "migrate"])
-    if is_need_exit:
-        logger.info("Migrations executed. Exiting...")
-        sys.exit(0)
 
 
-def execute_runserver():
+def execute_runserver(*args, **kwargs) -> None:
+    execute_migrations()
     HOST = environ.get("HOST", "0.0.0.0")
     PORT = environ.get("PORT", "3000")
     execute_from_command_line([__name__, "runserver", f"{HOST}:{PORT}"])
 
 
-def runserver():
+def check_connection(*args, **kwargs) -> None:
     for try_count in range(1, 6):
         try:
             connections["default"].cursor()
             logger.info(f"connected successfuly")
-            return execute_runserver
+            return
         except OperationalError as e:
             x = connections["default"].settings_dict
             error = (
@@ -68,8 +66,12 @@ def runserver():
 
 
 if __name__ == "__main__":
-    namespace = parse_args()
+    namespace: Namespace = parse_args()
+
     load_enviroments()
-    app_run = runserver()
-    execute_migrations(Modes[namespace.mode] is Modes.migration)
-    app_run()
+    check_connection()
+
+    if Modes[namespace.mode] is Modes.migration:
+        execute_migrations()
+    elif Modes[namespace.mode] is Modes.runserver:
+        execute_runserver()
