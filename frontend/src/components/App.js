89f7aebee138f4f0a26023e0a234ea0f75f1170f
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-01-04 17:19:56 +0100

    update main app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,98 +1,35 @@
-import Select from "./Select/Select";
+import { updateState } from "Reducers/mainReducer";
+import { useDispatch } from "react-redux";
 import React from "react";
 import axios from "axios";
 
+const apiAddress = "/expenses/";
+
 function App() {
-  const [shops, setShops] = React.useState([{ id: 0, name: "null" }]);
-  const [shop, setShop] = React.useState({ id: 0, name: "null" });
-  const [products, setProducts] = React.useState([{ id: 0, name: "null" }]);
-  const [product, setProduct] = React.useState({ id: 0, name: "null" });
-  const [subCategories, setSubCategories] = React.useState([
-    { id: 0, name: "null" },
-  ]);
-  const [subCategory, setSubCategory] = React.useState({ id: 0, name: "null" });
-  const [date, setDate] = React.useState(
-    new Date().toISOString().split("T")[0]
-  );
-  const [price, setPrice] = React.useState(0);
+  const dispatch = useDispatch();
 
   React.useEffect((_) => {
-    axios
-      .get("/expenses/shop/")
+    Promise.all([
+      axios.get(`${apiAddress}sub_category/`),
+      axios.get(`${apiAddress}transaction/`),
+      axios.get(`${apiAddress}product/`),
+      axios.get(`${apiAddress}shop/`),
+    ])
       .then((data) => {
-        setShops(data.data);
-        setShop(data.data[0]);
-      })
-      .catch((e) => console.log(e));
-    axios
-      .get("/expenses/product/")
-      .then((data) => {
-        setProducts(data.data);
-        setProduct(data.data[0]);
-      })
-      .catch((e) => console.log(e));
-    axios
-      .get("/expenses/sub_category/")
-      .then((data) => {
-        setSubCategories(data.data);
-        setSubCategory(data.data[0]);
+        const [subCategories, transactions, products, shops] = data;
+        dispatch(
+          updateState({
+            subCategories: subCategories.data,
+            transactions: transactions.data,
+            products: products.data,
+            shops: shops.data,
+          })
+        );
       })
       .catch((e) => console.log(e));
   }, []);
 
-  const sendHandler = (e) => {
-    e.preventDefault();
-    const newData = {
-      price: price,
-      shop: shop.id,
-      date: String(date),
-      product: product.id,
-      sub_category: subCategory.id,
-    };
-    axios
-      .post("/expenses/transaction/", newData)
-      .then((data) => {
-        console.log(data.data);
-      })
-      .catch((e) => console.log(e));
-    // console.log(newData);
-  };
-
-  return (
-    <div>
-      <form onSubmit={sendHandler}>
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <div style={{ width: "300px" }}>
-          <Select
-            changeHandler={(e) => setShop(e)}
-            items={shops}
-            value={shop}
-          />
-          <Select
-            changeHandler={(e) => setSubCategory(e)}
-            items={subCategories}
-            value={subCategory}
-          />
-          <Select
-            changeHandler={(e) => setProduct(e)}
-            items={products}
-            value={product}
-          />
-        </div>
-        <input
-          onChange={(e) => setPrice(e.target.value)}
-          value={price}
-          type="number"
-          step="any"
-        />
-        <button onClick={sendHandler}>send</button>
-      </form>
-    </div>
-  );
+  return <h1>App</h1>;
 }
 
 export default App;
