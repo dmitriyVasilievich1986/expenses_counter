d913d6b2fb0435bdff6c8cb80bf2e27d00adcb50
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2024-02-24 15:55:52 +0100

    update constants

--- frontend/src/components/Constants.js
+++ frontend/src/components/Constants.js
@@ -6,11 +6,13 @@ export const API_URLS = {
   sub_category: `${API_URL}sub_category/`,
   transaction: `${API_URL}transaction/`,
   address: `${API_URL}shop_address/`,
+  category: `${API_URL}category/`,
   product: `${API_URL}product/`,
   shop: `${API_URL}shop/`,
 };
 
 export const MODAL_PAGES = {
+  createNewCategory: "createNewCategory",
   newProductPage: "newProductPage",
   deletePage: "deletePage",
 };
