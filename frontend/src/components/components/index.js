583b65b42f8763cebdf5b4422494566599e4652c
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2024-02-28 13:54:12 +0100

    create Navbar component

--- frontend/src/components/components/index.js
+++ frontend/src/components/components/index.js
@@ -1,5 +1,6 @@
 import CreateTransactionPage from "./createTransactionPage/CreateTransactionPage";
 import CreateCategoryPage from "./createCategoryPage/CreateCategoryPage";
 import TransactionsPage from "./transactions/TransactionsPage";
+import Navbar from "./navbar";
 
-export { TransactionsPage, CreateCategoryPage, CreateTransactionPage };
+export { TransactionsPage, CreateCategoryPage, CreateTransactionPage, Navbar };
---
+++ frontend/src/components/components/navbar/Navbar.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import classnames from "classnames/bind";
+import style from "./style.scss";
+import React from "react";
+
+const cx = classnames.bind(style);
+
+function Navbar() {
+  return (
+    <div className={cx("navbar")}>
+      <div className={cx("wrapper")}>
+        <NavLink className={({ isActive }) => cx("link", { isActive })} to="/">
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => cx("link", { isActive })}
+          to="/create/category"
+        >
+          Categories
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
