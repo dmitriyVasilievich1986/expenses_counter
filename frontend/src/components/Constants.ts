204134728389fb8efbd401b9fc436fa82f1b7518
dmitriy <dmitriyvasil@gmail.com>
2024-12-22 12:46:47 +0100

    fear: add ts constants

---
+++ frontend/src/components/Constants.ts
@@ -0,0 +1,10 @@
+const API_URL = "/expenses/";
+
+export enum API_URLS {
+  SubCategory = `${API_URL}sub_category/`,
+  Transaction = `${API_URL}transaction/`,
+  Address = `${API_URL}shop_address/`,
+  Category = `${API_URL}category/`,
+  Product = `${API_URL}product/`,
+  Shop = `${API_URL}shop/`,
+}
