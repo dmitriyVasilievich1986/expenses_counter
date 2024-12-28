2e0d521667fc6a2988f4a83e3c6cb95c563e15e3
dmitriy <dmitriyvasil@gmail.com>
2024-12-28 16:21:01 +0100

    feat: add `Link` component

---
+++ frontend/src/components/components/link/Link.tsx
@@ -0,0 +1,45 @@
+import { NavLink, useSearchParams } from "react-router-dom";
+import React from "react";
+
+import { PagesURLs } from "../../Constants";
+import { UrlParamsType } from "./types";
+
+class Params implements UrlParamsType {
+  address?: string | null = undefined;
+  date?: string | null = undefined;
+
+  constructor(params?: UrlParamsType) {
+    const [searchParams] = useSearchParams();
+
+    this.date =
+      params?.currentDate === null
+        ? undefined
+        : searchParams.get("currentDate");
+    this.address =
+      params?.address === null ? undefined : searchParams.get("address");
+  }
+
+  toString() {
+    const filteredParams = Object.fromEntries(
+      Object.entries(this).filter(([_, v]) => !!v),
+    );
+    const params = new URLSearchParams(filteredParams).toString();
+    return params ? `?${params}` : "";
+  }
+}
+
+export function Link(props: {
+  to: PagesURLs | string;
+  params?: UrlParamsType;
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <NavLink
+      className={props.className}
+      to={{ pathname: props.to, search: new Params(props.params).toString() }}
+    >
+      {props.children}
+    </NavLink>
+  );
+}
