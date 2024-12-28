8d7ce3fe268f760e394af22e1e983e47c9fae5b2
dmitriy <dmitriyvasil@gmail.com>
2024-12-28 12:11:01 +0100

    fix: fix nested serializers

--- backend/main/views.py
+++ backend/main/views.py
@@ -6,6 +6,7 @@ from django.db.models import Count
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -39,6 +40,51 @@ def images_view(request: HttpRequest, pk: int | None = None) -> HttpResponse:
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductDetailedSerializer
     queryset = Product.objects.all()
+
+    def create(
+        self, request: HttpRequest, *args: tuple[Any], **kwargs: dict[str, Any]
+    ) -> HttpResponse:
+        category = Category.objects.get(pk=request.data["sub_category"])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer._validated_data["sub_category"] = category
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def list(
+        self, request: HttpRequest, *args: tuple[Any], **kwargs: dict[str, Any]
+    ) -> HttpResponse:
+        queryset = self.filter_queryset(self.get_queryset()).order_by("-sub_category")
+
+        if (page := self.paginate_queryset(queryset)) is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def update(
+        self, request: HttpRequest, *args: tuple[Any], **kwargs: dict[str, Any]
+    ) -> HttpResponse:
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if "sub_category" in request.data:
+            serializer._validated_data["sub_category"] = Category.objects.get(
+                pk=request.data["sub_category"]
+            )
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
     @action(detail=False, methods=["post"])
     def price(self, request: HttpRequest) -> HttpResponse:
@@ -103,5 +149,51 @@ class ShopViewSet(ModelViewSet):
 
 
 class TransactionViewSet(ModelViewSet):
-    serializer_class = TransactionDetailedSerializer
+    serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
+
+    def create(
+        self, request: HttpRequest, *args: tuple[Any], **kwargs: dict[str, Any]
+    ) -> HttpResponse:
+        address = ShopAddress.objects.get(pk=request.data["address"])
+        product = Product.objects.get(pk=request.data["product"])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer._validated_data["address"] = address
+        serializer._validated_data["product"] = product
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def retrieve(
+        self, request: HttpRequest, *args: tuple[Any], **kwargs: dict[str, Any]
+    ) -> HttpResponse:
+        instance = self.get_object()
+        serializer = TransactionDetailedSerializer(instance)
+        return Response(serializer.data)
+
+    def update(
+        self, request: HttpRequest, *args: tuple[Any], **kwargs: dict[str, Any]
+    ) -> HttpResponse:
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if "address" in request.data:
+            serializer._validated_data["address"] = ShopAddress.objects.get(
+                pk=request.data["address"]
+            )
+        if "product" in request.data:
+            serializer._validated_data["product"] = Product.objects.get(
+                pk=request.data["product"]
+            )
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
