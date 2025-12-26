7ff4a155efc3b64982707f9af118afeb1d2ba27f
dmitriy <dmitriyvasil@gmail.com>
2025-12-26 18:59:56 +0100

    chore: Remove unused project files and configurations

--- tests/test_products.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from expense_counter.main.models import Product
-
-
-@pytest.mark.django_db
-def test_products_count() -> None:
-    products_count = Product.objects.all().count()
-    assert products_count == 0
