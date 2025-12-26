d5ea465e54d364494841752de23496510da680e6
dmitriy <dmitriyvasil@gmail.com>
2025-12-26 16:08:47 +0100

    refactor: update Base model to use SQLAlchemy registry and metadata

--- backend/src/expenses_counter/services/database/models/base.py
+++ backend/src/expenses_counter/services/database/models/base.py
@@ -2,10 +2,14 @@
 
 __all__ = ("Base",)
 
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, registry
+from sqlalchemy.schema import MetaData
+
+mapper_registry = registry(metadata=MetaData())
 
 
 class Base(DeclarativeBase):
     """Base model class for all SQLAlchemy models."""
 
-    pass
+    registry = mapper_registry
+    metadata = mapper_registry.metadata
