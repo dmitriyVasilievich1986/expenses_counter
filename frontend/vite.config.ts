8b5a884a13030e061e2b69dcc64561e269897d12
dmitriy <dmitriyvasil@gmail.com>
2025-12-27 14:31:56 +0100

    feat: replace webpack with vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "./src/components"),
+    },
+  },
+  build: {
+    outDir: "../static",
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
+});
