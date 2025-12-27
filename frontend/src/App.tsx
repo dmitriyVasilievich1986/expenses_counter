cadd0a67beab1780d033a30b171f199a46277260
dmitriy <dmitriyvasil@gmail.com>
2025-12-27 15:45:55 +0100

    feat: create basic frontend application structure with App component and routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Navbar } from "@components";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <p>APP</p>
+    </>
+  );
+}
+
+export default App;
