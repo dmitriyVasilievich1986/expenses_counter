2ebd4430d25a36d9b881167be3388349c6484bb8
dmitriy <dmitriyvasil@gmail.com>
2025-12-28 13:31:35 +0100

    refactor: simplify category dao by removing redundant schemas and methods, and introduce recursive category loading

--- backend/src/expenses_counter/services/daos/category/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Category schemas module."""
-
-from .get import CategoryGet
-from .patch import CategoryPatch
-from .post import CategoryPost
-from .put import CategoryPut
-
-__all__ = ("CategoryGet", "CategoryPatch", "CategoryPost", "CategoryPut")
