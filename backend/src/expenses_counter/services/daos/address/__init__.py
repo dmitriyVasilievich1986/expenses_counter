0177bf7643a95a15164e5f77c5a713009875564b
dmitriy <dmitriyvasil@gmail.com>
2025-12-28 20:19:16 +0100

    refactor: simplify address dao by removing redundant schemas and methods, and introduce new address dao implementation

--- backend/src/expenses_counter/services/daos/address/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Address DAOs module."""
-
-from .dao import AddressDAO
-from .schemas import AddressGet, AddressPatch, AddressPost, AddressPut
-
-__all__ = ("AddressDAO", "AddressGet", "AddressPatch", "AddressPost", "AddressPut")
--- backend/src/expenses_counter/services/daos/address/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Address schemas module."""
-
-from .get import AddressGet
-from .patch import AddressPatch
-from .post import AddressPost
-from .put import AddressPut
-
-__all__ = ("AddressGet", "AddressPatch", "AddressPost", "AddressPut")
