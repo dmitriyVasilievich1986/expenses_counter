8923b0ab0dbb70e8c91f11e6ccc9c1adeb1a47ab
dmitriy <dmitriyvasil@gmail.com>
2025-12-28 20:48:53 +0100

    refactor: simplify transaction dao by removing redundant schemas and methods, and introduce new transaction dao implementation

--- backend/src/expenses_counter/services/daos/transaction/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Transaction schemas module."""
-
-from .get import TransactionGet
-from .patch import TransactionPatch
-from .post import TransactionPost
-from .put import TransactionPut
-
-__all__ = ("TransactionGet", "TransactionPatch", "TransactionPost", "TransactionPut")
