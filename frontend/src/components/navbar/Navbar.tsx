46d6d2aa48ce22faed80d938082174e70d784f12
dmitriy <dmitriyvasil@gmail.com>
2025-12-29 22:05:35 +0100

    feat: add link to shops in Navbar component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
-import AppBar from "@mui/material/AppBar";
-import Container from "@mui/material/Container";
-import Toolbar from "@mui/material/Toolbar";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import classnames from "classnames/bind";
-import { Link } from "react-router";
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import classnames from 'classnames/bind';
+import { Link } from 'react-router';
 
-import * as defaultStyle from "./style.scss";
+import * as defaultStyle from './style.scss';
 
 const cx = classnames.bind(defaultStyle);
 
@@ -16,8 +16,11 @@ export function Navbar() {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Stack direction="row" spacing={2}>
-            <Link to="/" className={cx("navlink-text")}>
+            <Link to="/" className={cx('navlink-text')}>
               <Typography variant="h6">Home</Typography>
+            </Link>
+            <Link to="/shop" className={cx('navlink-text')}>
+              <Typography variant="h6">Shops</Typography>
             </Link>
           </Stack>
         </Toolbar>
