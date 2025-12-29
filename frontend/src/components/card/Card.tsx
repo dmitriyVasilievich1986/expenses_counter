337881d0473e340e0c6df730e732a959c8f84f15
dmitriy <dmitriyvasil@gmail.com>
2025-12-29 22:03:38 +0100

    feat: add Card component to frontend with customizable title, description, and icon

---
+++ frontend/src/components/card/Card.tsx
@@ -0,0 +1,39 @@
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import { default as MuiCard } from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import classnames from 'classnames/bind';
+
+import * as defaultStyle from './style.scss';
+
+const cx = classnames.bind(defaultStyle);
+
+export function Card(props: { title: string; description?: string | null; icon?: string | null }) {
+  return (
+    <MuiCard className={cx('card')}>
+      <CardHeader
+        avatar={
+          props.icon ? (
+            <img src={props.icon} style={{ width: '30px', height: '30px' }} />
+          ) : (
+            <StorefrontIcon />
+          )
+        }
+        sx={{ overflow: 'hidden' }}
+        slotProps={{
+          title: {
+            variant: 'h6',
+            gutterBottom: true,
+            sx: {
+              textOverflow: 'ellipsis',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+            },
+          },
+          subheader: { variant: 'caption' },
+        }}
+        title={props.title}
+        subheader={props.description}
+      />
+    </MuiCard>
+  );
+}
