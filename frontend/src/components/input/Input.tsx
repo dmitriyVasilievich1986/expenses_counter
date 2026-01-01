508b6c5e2c6bda3d13f55d805627b1e6b27c41ab
dmitriy <dmitriyvasil@gmail.com>
2026-01-01 10:06:01 +0100

    feat: add reusable Input component

---
+++ frontend/src/components/input/Input.tsx
@@ -0,0 +1,25 @@
+import TextField from '@mui/material/TextField';
+import { useMainStore } from '@store/main';
+import { useMemo, useState } from 'react';
+
+export function Input(props: { label: string; name: string; defaultValue?: string | null }) {
+  const [value, setValue] = useState<string>(props.defaultValue ?? '');
+  const isLoading = useMainStore((state) => state.isLoading);
+  const isChanged = useMemo(
+    () => props.defaultValue && value !== props.defaultValue,
+    [props.defaultValue, value]
+  );
+
+  return (
+    <TextField
+      label={props.label}
+      name={props.name}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      fullWidth
+      disabled={isLoading}
+      color={isChanged ? 'warning' : 'primary'}
+      focused={isChanged ? true : undefined}
+    />
+  );
+}
