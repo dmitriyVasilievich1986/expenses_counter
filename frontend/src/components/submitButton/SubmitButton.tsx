896253450f1ed86c6e0e52a24a0d5381907a7ab1
dmitriy <dmitriyvasil@gmail.com>
2026-01-10 09:40:56 +0100

    refactor: enhance SubmitButton component with variant and onClick props

--- frontend/src/components/submitButton/SubmitButton.tsx
+++ frontend/src/components/submitButton/SubmitButton.tsx
@@ -2,20 +2,46 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { useMainStore } from '@store/main';
 import Button from '@mui/material/Button';
 
-export function SubmitButton(props: { disabled?: boolean; isSubmit?: boolean }) {
+export function SubmitButton(props: {
+  disabled?: boolean;
+  variant?: 'create' | 'update' | 'delete';
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}) {
   const isLoading = useMainStore((state) => state.isLoading);
+
+  const getVariant = () => {
+    switch (props.variant) {
+      case 'create':
+        return 'primary';
+      case 'update':
+        return 'secondary';
+      case 'delete':
+        return 'error';
+    }
+  };
+
+  const getButtonText = () => {
+    switch (props.variant) {
+      case 'create':
+        return 'Create';
+      case 'update':
+        return 'Update';
+      case 'delete':
+        return 'Delete';
+    }
+  };
 
   if (isLoading) {
     return <CircularProgress size={20} />;
   }
   return (
     <Button
-      type="submit"
       variant="contained"
-      color={props.isSubmit ? 'primary' : 'secondary'}
+      color={getVariant()}
       disabled={props.disabled}
+      onClick={props.onClick}
     >
-      {props.isSubmit ? 'Create' : 'Update'}
+      {getButtonText()}
     </Button>
   );
 }
