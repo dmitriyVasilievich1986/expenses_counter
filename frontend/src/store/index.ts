1863e9f768f819a0e54d382fc97b4e5f8a6623f4
dmitriy <dmitriyvasil@gmail.com>
2026-01-10 10:58:05 +0100

    fix: reorder import statements in store index

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,6 @@
 import { useCategoryStore } from './category';
 import { useMainStore } from './main';
-import { useShopStore } from './shop';
 import { useProductStore } from './product';
+import { useShopStore } from './shop';
 
 export { useShopStore, useCategoryStore, useMainStore, useProductStore };
