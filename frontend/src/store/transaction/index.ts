07bd4f4d1d8c408e444a275aaeb1d8da8bf88f88
dmitriy <dmitriyvasil@gmail.com>
2026-01-11 11:33:23 +0100

    feat: introduce transaction store with zustand and add transaction types

---
+++ frontend/src/store/transaction/index.ts
@@ -0,0 +1,5 @@
+import { useTransactionStore } from './transactionStore';
+
+import type { TransactionSimpleType, TransactionType } from './types';
+
+export { useTransactionStore, type TransactionSimpleType, type TransactionType };
---
+++ frontend/src/store/transaction/transactionStore.ts
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs';
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+import type { TransactionType, TransactionStoreStateType } from './types';
+
+export const useTransactionStore = create<TransactionStoreStateType>()(
+  devtools((set) => ({
+    transactions: null,
+    currentDate: dayjs(),
+    currentTransaction: null,
+    setCurrentDate: (date) => set({ currentDate: date }, undefined, 'setCurrentDate'),
+    setCurrentTransaction: (transaction) =>
+      set({ currentTransaction: transaction }, undefined, 'setCurrentTransaction'),
+    setTransactions: (transactions) =>
+      set({ transactions: transactions }, undefined, 'setTransactions'),
+    addTransactions: (transactions) =>
+      set(
+        (state) => ({ transactions: [...(state.transactions ?? []), ...transactions] }),
+        undefined,
+        'addTransactions'
+      ),
+    updateTransaction: (transaction) =>
+      set(
+        (state) => {
+          return {
+            transactions: (state.transactions as TransactionType[]).map((t) =>
+              t.id === transaction.id ? transaction : t
+            ),
+          };
+        },
+        undefined,
+        'updateTransaction'
+      ),
+    deleteTransaction: (id) =>
+      set(
+        (state) => {
+          return {
+            transactions: (state.transactions as TransactionType[]).filter((t) => t.id !== id),
+          };
+        },
+        undefined,
+        'deleteTransaction'
+      ),
+  }))
+);
