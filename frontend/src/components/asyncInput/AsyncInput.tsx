be1db5df4b90ca2b6f5fc77bdb634382a2c8117a
dmitriy <dmitriyvasil@gmail.com>
2026-01-11 11:30:08 +0100

    feat: introduce AsyncInput component for asynchronous input handling

---
+++ frontend/src/components/asyncInput/AsyncInput.tsx
@@ -0,0 +1,60 @@
+import Autocomplete from '@mui/material/Autocomplete';
+import CircularProgress from '@mui/material/CircularProgress';
+import TextField from '@mui/material/TextField';
+import { useEffect } from 'react';
+
+import { useMainStore } from '@store/main';
+
+export function AsyncInput<I>(props: {
+  value: I | null;
+  onChange: (value: I) => void;
+  items: I[] | null;
+  getItems: () => Promise<I[]>;
+  label: string;
+  nameColumn?: keyof I;
+}) {
+  const nameColumn = (props.nameColumn ?? 'name') as keyof I;
+  const isLoading = useMainStore((state) => state.isLoading);
+
+  useEffect(() => {
+    if (props.value === null) {
+      props.getItems().then((data) => {
+        props.onChange(data[0] as I);
+      });
+    }
+  }, [props.items]);
+
+  const handleOpen = async () => {
+    await props.getItems();
+  };
+
+  if (props.value === null) return null;
+  return (
+    <Autocomplete
+      options={props.items ?? [props.value]}
+      getOptionLabel={(option) => (option as I)[nameColumn] as string}
+      value={props.value}
+      onOpen={handleOpen}
+      getOptionKey={(option) => (option as unknown as { id: number }).id}
+      onChange={(_, v) => props.onChange(v as I)}
+      disableClearable
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={props.label}
+          slotProps={{
+            input: {
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {isLoading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            },
+          }}
+        />
+      )}
+    />
+  );
+}
---
+++ frontend/src/components/asyncInput/index.ts
@@ -0,0 +1,3 @@
+import { AsyncInput } from './AsyncInput';
+
+export { AsyncInput };
