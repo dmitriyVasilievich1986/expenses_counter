65a69d0aaf514c226733ff9cd8183e1ec7bad62a
dmitriy <dmitriyvasil@gmail.com>
2026-01-26 11:59:24 +0100

    refactor: remove synchronous DatabaseClient in favor of AsyncDatabaseClient

--- backend/src/expenses_counter/services/database/__init__.py
+++ backend/src/expenses_counter/services/database/__init__.py
@@ -3,8 +3,7 @@
 from sqlalchemy.orm import declarative_base
 
 from .async_client import AsyncDatabaseClient
-from .client import DatabaseClient
 
 Base = declarative_base()
 
-__all__ = ("AsyncDatabaseClient", "Base", "DatabaseClient")
+__all__ = ("AsyncDatabaseClient", "Base")
