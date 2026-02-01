656a82227541de1030120c8ef9ca12312adb40e4
dmitriy <dmitriyvasil@gmail.com>
2026-02-01 20:05:15 +0100

    feat: add methods to TransactionDAO for spendings and popular products

--- backend/src/expenses_counter/services/daos/transaction_dao.py
+++ backend/src/expenses_counter/services/daos/transaction_dao.py
@@ -3,7 +3,10 @@
 __all__ = ("TransactionDAO",)
 
 
+from sqlalchemy import func, select
+
 from expenses_counter.services.daos.base import BaseDAO
+from expenses_counter.services.database.models.product import Product
 from expenses_counter.services.database.models.transaction import Transaction
 
 
@@ -18,3 +21,39 @@ class TransactionDAO(BaseDAO[Transaction]):
     database_model = Transaction
     select_in_options_single = (Transaction.product, Transaction.address)
     select_in_options_all = (Transaction.product, Transaction.address)
+
+    @BaseDAO.error_handler
+    async def get_spendings_grouped_by_month(self) -> list[tuple[str, float]]:
+        """Get the spendings grouped by month.
+
+        Returns:
+            A list of tuples containing the month (YYYY-MM format) and the spendings.
+
+        """
+        month_column = func.to_char(Transaction.date, "YYYY-MM-01")
+        stmt = select(month_column, func.sum(Transaction.price)).group_by(month_column).order_by(month_column)
+        result = await self.session.execute(stmt)
+        return result.all()
+
+    @BaseDAO.error_handler
+    async def get_most_poular_products(self, limit: int = 10) -> list[Product]:
+        """Get the most popular products.
+
+        Args:
+            limit: The number of products to return.
+
+        Returns:
+            A list of products.
+
+        """
+        amount = func.count(Transaction.product_id)
+        subquery = (
+            select(Transaction.product_id)
+            .group_by(Transaction.product_id)
+            .order_by(amount.desc())
+            .limit(limit)
+            .subquery()
+        )
+        stmt = select(Product).where(Product.id.in_(select(subquery.c.product_id)))
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
