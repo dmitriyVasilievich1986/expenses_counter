f86a5c7ce352168593cb3d1644ea3643e038f154
dmitriy <dmitriyvasil@gmail.com>
2026-04-23 16:29:29 +0200

    feat: Add PatchAddressBody schema for address updates

--- backend/src/expenses_counter/modules/routers/schemas/requests/address/__init__.py
+++ backend/src/expenses_counter/modules/routers/schemas/requests/address/__init__.py
@@ -1,7 +1,8 @@
 """Address request schemas module."""
 
+__all__ = ("GetAllAddressesQuery", "PatchAddressBody", "PostAddressBody", "PutAddressBody")
+
 from .get_all_addresses_query import GetAllAddressesQuery
+from .patch_address_body import PatchAddressBody
 from .post_address_body import PostAddressBody
 from .put_address_body import PutAddressBody
-
-__all__ = ("GetAllAddressesQuery", "PostAddressBody", "PutAddressBody")
