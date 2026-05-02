255787fbb7a2c4c0e655206152d4f87d4ebaa16a
dmitriy <dmitriyvasil@gmail.com>
2026-05-02 10:23:24 +0200

    fix: Update superclass initialization in TableParser to use modern syntax with type ignore comment

--- backend/src/expenses_counter/utils/web_crawler/table_parser.py
+++ backend/src/expenses_counter/utils/web_crawler/table_parser.py
@@ -40,7 +40,7 @@ class TableParser(pd.DataFrame):
         df = dfs[0]
         df = self._rename_columns(df)
         df = self._convert_dataframe(df)
-        super(pd.DataFrame, self).__init__(df)
+        super().__init__(df)  # type: ignore[call-arg]
 
     def _rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
         """Rename Serbian column names to English equivalents.
