50897d8084c21bc90271c87ea24804dcf787e80a
dmitriy <dmitriyvasil@gmail.com>
2026-05-12 20:47:46 +0200

    feat: Add request interceptor for authentication token management
    * Implemented a request interceptor in the API client to manage authentication tokens.
    * The interceptor checks for the presence of an access token in cookies and redirects to the login page if not found.
    * If an access token is present, it is added to the request headers for authorization, enhancing security for API calls.

--- frontend/src/services/apiClient/base.ts
+++ frontend/src/services/apiClient/base.ts
@@ -1,12 +1,31 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 import { useMainStore } from '@store/main';
+
 
 export const apiClientInstance = axios.create({
   baseURL: import.meta.env.VITE_API_HOST,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+// Request interceptor - set auth token
+apiClientInstance.interceptors.request.use((config) => {
+  const accessToken = Cookies.get('accessToken');
+
+  if (!accessToken) {
+    const fullRedirectUrl = `/login?redirectTo=${encodeURIComponent(window.location.pathname)}`;
+    window.location.href = fullRedirectUrl;
+    throw new Error('Unauthorized');
+  }
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
 
 export const useApiClientWrapper = () => {
