1a232cafd9508fc861dae894ebb280c2cf89b411
dmitriy <dmitriyvasil@gmail.com>
2026-05-12 20:47:57 +0200

    feat: Implement authentication API client with login functionality
    * Added a new API client for authentication, including a login method that handles user credentials and returns a login response with an access token and expiration date.
    * Created types for the login response to ensure type safety and clarity in API interactions.
    * Established a public entry point for the authentication API client to facilitate usage across the application.

---
+++ frontend/src/services/apiClient/auth/client.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+import type { LoginResponse } from './types';
+
+export const useAuthAPIClient = () => {
+  return {
+    login: async (username: string, password: string) => {
+      const response = await axios.post<LoginResponse>(
+        `${import.meta.env.VITE_API_HOST}/login`,
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      return response.data;
+    },
+  };
+};
