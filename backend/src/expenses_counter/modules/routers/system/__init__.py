db05d44a9736a4c71ff8958ffa13bcce9d4f3644
dmitriy <dmitriyvasil@gmail.com>
2026-05-13 09:38:08 +0200

    feat: Update API routing and authentication endpoints
    * Modified the FastAPI router to include a prefix for API endpoints, enhancing organization and clarity.
    * Updated the login endpoint in the frontend API client to reflect the new API path.
    * Removed the deprecated Nginx location block for the login endpoint, streamlining the configuration for backend routing.

--- backend/src/expenses_counter/modules/routers/system/__init__.py
+++ backend/src/expenses_counter/modules/routers/system/__init__.py
@@ -9,7 +9,7 @@ from .index import router as index_router
 from .login import router as login_router
 from .version import router as version_router
 
-router = APIRouter(tags=["System"])
+router = APIRouter(prefix="/api", tags=["System"])
 
 router.include_router(version_router)
 router.include_router(health_router)
--- frontend/src/services/apiClient/auth/client.ts
+++ frontend/src/services/apiClient/auth/client.ts
@@ -6,7 +6,7 @@ export const useAuthAPIClient = () => {
   return {
     login: async (username: string, password: string) => {
       const response = await axios.post<LoginResponse>(
-        `${import.meta.env.VITE_API_HOST}/login`,
+        `${import.meta.env.VITE_API_HOST}/api/login`,
         {
           username,
           password,
