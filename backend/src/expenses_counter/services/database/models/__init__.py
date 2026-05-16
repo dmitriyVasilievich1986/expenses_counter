871ad67520b228b82088e0c8ad9f1f85a337421b
dmitriy <dmitriyvasil@gmail.com>
2026-05-16 13:34:00 +0200

    feat: Update __all__ exports in models module to include User
    - Added User to the __all__ exports in the models module to ensure proper module organization and accessibility.
    - Removed the previous export of Transaction from __all__, streamlining the module's public interface.

--- backend/src/expenses_counter/services/database/models/__init__.py
+++ backend/src/expenses_counter/services/database/models/__init__.py
@@ -1,9 +1,10 @@
 """Models module."""
 
+__all__ = ("Address", "Category", "Product", "Shop", "Transaction", "User")
+
 from .address import Address
 from .category import Category
 from .product import Product
 from .shop import Shop
 from .transaction import Transaction
-
-__all__ = ("Address", "Category", "Product", "Shop", "Transaction")
+from .user import User
